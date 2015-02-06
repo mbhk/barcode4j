dfe793425e3d88959afbd5573bf49205c839aaf7
Michael Krueger <mbh.krueger@gmail.com>
2015-02-06 11:29:51 +0100

    some minor improvements to playground app

--- barcode4j-playground/src/main/java/org/krysalis/barcode4j/playground/BarcodePanel.java
+++ barcode4j-playground/src/main/java/org/krysalis/barcode4j/playground/BarcodePanel.java
@@ -17,10 +17,8 @@ package org.krysalis.barcode4j.playground;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.GraphicsEnvironment;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
@@ -31,52 +29,48 @@ import org.krysalis.barcode4j.BarcodeDimension;
 import org.krysalis.barcode4j.BarcodeGenerator;
 import org.krysalis.barcode4j.output.CanvasProvider;
 import org.krysalis.barcode4j.output.java2d.Java2DCanvasProvider;
-import org.krysalis.barcode4j.tools.UnitConv;
 
 /**
  * @version $Id$
  */
 public class BarcodePanel extends JPanel {
+    private static final long serialVersionUID = 3864384667358207962L;
 
     private BarcodeGenerator bargen;
-    private String msg;
+    private String msg = "";
+    private int orientation = 0;
 
-    public void setBarcode(BarcodeGenerator bargen, String msg) {
+    public void setBarcode(BarcodeGenerator bargen) {
         this.bargen = bargen;
-        this.msg = msg;
+        repaint();
+    }
+    
+    public void setMessage(String message) {
+        this.msg = message;
+        repaint();
+    }
+    
+    public void setOrientation(int orientation) {
+        this.orientation = orientation;
+        repaint();
     }
 
-    /**
-     * @see javax.swing.JComponent#paintComponent(Graphics)
-     */
+    @Override
     protected void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
         Rectangle rect = new Rectangle(30, 30, getWidth() - 60, getHeight() - 60);
         paintBarcode(graphics, rect);
         graphics.setColor(Color.blue);
         graphics.drawRect((int)rect.getX(), (int)rect.getY(), (int)rect.getWidth(), (int)rect.getHeight());
-
-        if (false) {
-            Font f = new Font("Tahoma", Font.PLAIN, (int)Math.round(UnitConv.pt2mm(80)));
-            graphics.setFont(f);
-            graphics.drawString("123 Test Text", 50, 50);
-
-            f = new Font("OCR-B-10 BT", Font.PLAIN, (int)Math.round(UnitConv.pt2mm(80)));
-            graphics.setFont(f);
-            graphics.drawString('\0'+'\1'+"123 Test Text", 50, 100);
-            String[] fontlist = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
-            for (int i=0; i<fontlist.length; i++) System.out.println(fontlist[i]);
-        }
     }
 
     protected void paintBarcode(Graphics graphics, Rectangle rect) {
-        //System.out.println("printBarcode: " + rect);
         final Dimension dim = rect.getSize();
         final Graphics2D g2d = (Graphics2D)graphics;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
         
-        CanvasProvider provider = new Java2DCanvasProvider(g2d, 270);
+        CanvasProvider provider = new Java2DCanvasProvider(g2d, orientation);
         
         AffineTransform baktrans = g2d.getTransform();
         g2d.translate(rect.getX(), rect.getY());
--- barcode4j-playground/src/main/java/org/krysalis/barcode4j/playground/PlaygroundFrame.java
+++ barcode4j-playground/src/main/java/org/krysalis/barcode4j/playground/PlaygroundFrame.java
@@ -15,52 +15,54 @@
  */
 package org.krysalis.barcode4j.playground;
 
-import java.awt.Frame;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import org.apache.avalon.framework.configuration.ConfigurationException;
 
 import org.apache.avalon.framework.configuration.DefaultConfiguration;
+import org.krysalis.barcode4j.BarcodeException;
 import org.krysalis.barcode4j.BarcodeGenerator;
 import org.krysalis.barcode4j.BarcodeUtil;
 
 /**
  * @version $Id$
  */
-public class PlaygroundFrame extends Frame {
+public class PlaygroundFrame extends JFrame {
 
     public static final String TITLE = "Barcode Playground";
-    
-    public PlaygroundFrame() {
-        super(TITLE);
-        addWindowListener(new WindowHandler());
-        buildGUI();
-        setSize(500, 400);
-    }
-    
+    private static final long serialVersionUID = 3241340455560147586L;
+    private static final Logger LOGGER = Logger.getLogger(PlaygroundFrame.class.getName());
+
     private void buildGUI() {
+        setTitle(TITLE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(500, 400);
         BarcodePanel bcpanel = new BarcodePanel();
         add("Center", bcpanel);
-        
+
         try {
-            DefaultConfiguration cfg = new DefaultConfiguration("ean-13");
+            DefaultConfiguration cfg = new DefaultConfiguration("code128");
             DefaultConfiguration child = new DefaultConfiguration("human-readable-font");
             //child.setValue("OCR-B 10 Pitch BT");
             child.setValue("Tahoma");
             cfg.addChild(child);
-            
-            BarcodeGenerator gen = 
-                    BarcodeUtil.getInstance().createBarcodeGenerator(cfg);
-            
-            bcpanel.setBarcode(gen, "419458670510+06");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-    private class WindowHandler extends WindowAdapter {
-        public void windowClosing(WindowEvent we) {
-            System.exit(0);
+
+            BarcodeGenerator gen
+                    = BarcodeUtil.getInstance().createBarcodeGenerator(cfg);
+
+            bcpanel.setBarcode(gen);
+            bcpanel.setMessage("Hello World!");
+        } catch (ConfigurationException e) {
+            LOGGER.log(Level.SEVERE, "Configuration Exception", e);
+        } catch (BarcodeException e) {
+            LOGGER.log(Level.SEVERE, "Barcode Exception", e);
         }
     }
 
+    public void createAndShowGUI() {
+        buildGUI();
+        setVisible(true);
+    }
 }
