620e1b0d2731902473eabec66bebfb96b8469631
Michael Krueger <mbh.krueger@gmail.com>
2015-02-06 13:51:49 +0100

    Playground is playable.

--- barcode4j-playground/src/main/java/org/krysalis/barcode4j/playground/BarcodePanel.java
+++ barcode4j-playground/src/main/java/org/krysalis/barcode4j/playground/BarcodePanel.java
@@ -22,11 +22,17 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JPanel;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.configuration.DefaultConfiguration;
 
 import org.krysalis.barcode4j.BarcodeDimension;
+import org.krysalis.barcode4j.BarcodeException;
 import org.krysalis.barcode4j.BarcodeGenerator;
+import org.krysalis.barcode4j.BarcodeUtil;
 import org.krysalis.barcode4j.output.CanvasProvider;
 import org.krysalis.barcode4j.output.java2d.Java2DCanvasProvider;
 
@@ -34,22 +40,35 @@ import org.krysalis.barcode4j.output.java2d.Java2DCanvasProvider;
  * @version $Id$
  */
 public class BarcodePanel extends JPanel {
+
     private static final long serialVersionUID = 3864384667358207962L;
 
     private BarcodeGenerator bargen;
     private String msg = "";
     private int orientation = 0;
 
-    public void setBarcode(BarcodeGenerator bargen) {
-        this.bargen = bargen;
-        repaint();
+    public void setBarcodeName(String name) {
+        DefaultConfiguration cfg = new DefaultConfiguration(name);
+        DefaultConfiguration child = new DefaultConfiguration("human-readable-font");
+        child.setValue("Tahoma");
+        cfg.addChild(child);
+
+        try {
+            BarcodeGenerator gen
+                    = BarcodeUtil.getInstance().createBarcodeGenerator(cfg);
+            setBarcode(gen);
+        } catch (ConfigurationException ex) {
+            Logger.getLogger(BarcodePanel.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (BarcodeException ex) {
+            Logger.getLogger(BarcodePanel.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
     public void setMessage(String message) {
         this.msg = message;
         repaint();
     }
-    
+
     public void setOrientation(int orientation) {
         this.orientation = orientation;
         repaint();
@@ -59,19 +78,26 @@ public class BarcodePanel extends JPanel {
     protected void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
         Rectangle rect = new Rectangle(30, 30, getWidth() - 60, getHeight() - 60);
-        paintBarcode(graphics, rect);
-        graphics.setColor(Color.blue);
-        graphics.drawRect((int)rect.getX(), (int)rect.getY(), (int)rect.getWidth(), (int)rect.getHeight());
+        try {
+            paintBarcode(graphics, rect);
+        } catch (Exception e) {
+            paintError(graphics, rect);
+        }
     }
 
-    protected void paintBarcode(Graphics graphics, Rectangle rect) {
+    private void setBarcode(BarcodeGenerator bargen) {
+        this.bargen = bargen;
+        repaint();
+    }
+
+    private void paintBarcode(Graphics graphics, Rectangle rect) {
         final Dimension dim = rect.getSize();
-        final Graphics2D g2d = (Graphics2D)graphics;
+        final Graphics2D g2d = (Graphics2D) graphics;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-        
+
         CanvasProvider provider = new Java2DCanvasProvider(g2d, orientation);
-        
+
         AffineTransform baktrans = g2d.getTransform();
         g2d.translate(rect.getX(), rect.getY());
         BarcodeDimension barDim = bargen.calcDimensions(msg);
@@ -79,11 +105,14 @@ public class BarcodePanel extends JPanel {
         double sc1 = dim.getWidth() / barDim.getWidthPlusQuiet(provider.getOrientation());
         double sc2 = dim.getHeight() / barDim.getHeightPlusQuiet(provider.getOrientation());
         g2d.scale(sc1, sc2);
-        
+
         g2d.setColor(Color.black);
-        
+
         bargen.generateBarcode(provider, msg);
         g2d.setTransform(baktrans);
     }
 
+    private void paintError(Graphics graphics, Rectangle rect) {
+        graphics.drawString("ERROR", 0, 0);
+    }
 }
--- barcode4j-playground/src/main/java/org/krysalis/barcode4j/playground/PlaygroundFrame.java
+++ barcode4j-playground/src/main/java/org/krysalis/barcode4j/playground/PlaygroundFrame.java
@@ -15,54 +15,135 @@
  */
 package org.krysalis.barcode4j.playground;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Collection;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
 import javax.swing.WindowConstants;
-import org.apache.avalon.framework.configuration.ConfigurationException;
-
-import org.apache.avalon.framework.configuration.DefaultConfiguration;
-import org.krysalis.barcode4j.BarcodeException;
-import org.krysalis.barcode4j.BarcodeGenerator;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import org.krysalis.barcode4j.BarcodeUtil;
 
 /**
  * @version $Id$
  */
-public class PlaygroundFrame extends JFrame {
+public class PlaygroundFrame extends JFrame implements ActionListener, ChangeListener {
 
     public static final String TITLE = "Barcode Playground";
     private static final long serialVersionUID = 3241340455560147586L;
-    private static final Logger LOGGER = Logger.getLogger(PlaygroundFrame.class.getName());
+
+    private BarcodePanel bcpanel = null;
+    private JComboBox<String> barcodeNames = null;
+    private JTextField messageField = null;
+    private JSlider orientation = null;
+
+    private BarcodePanel getBarcodePanel() {
+        if (bcpanel == null) {
+            bcpanel = new BarcodePanel();
+        }
+        return bcpanel;
+    }
+
+    private JComboBox<String> getBarcodeNames() {
+        if (barcodeNames == null) {
+            barcodeNames = new JComboBox<String>();
+            Collection names = BarcodeUtil.getInstance().getClassResolver().getBarcodeNames();
+            for (Object name : names) {
+                barcodeNames.addItem((String) name);
+            }
+            barcodeNames.setSelectedItem("qr");
+            barcodeNames.addActionListener(this);
+        }
+        return barcodeNames;
+    }
+
+    private JTextField getMessageField() {
+        if (messageField == null) {
+            messageField = new JTextField("0123456");
+            messageField.getDocument().addDocumentListener(new DocumentListener() {
+
+                @Override
+                public void insertUpdate(DocumentEvent e) {
+                    update();
+                }
+
+                @Override
+                public void removeUpdate(DocumentEvent e) {
+                    update();
+                }
+
+                @Override
+                public void changedUpdate(DocumentEvent e) {
+                    update();
+                }
+
+                private void update() {
+                    getBarcodePanel().setMessage(getMessageField().getText());
+                }
+            });
+        }
+        return messageField;
+    }
+
+    private JSlider getOrientation() {
+        if (orientation == null) {
+            orientation = new JSlider(0, 270, 0);
+            orientation.setMinorTickSpacing(90);
+            orientation.setPaintTicks(true);
+            orientation.setSnapToTicks(true);
+            orientation.addChangeListener(this);
+        }
+        return orientation;
+    }
 
     private void buildGUI() {
         setTitle(TITLE);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(500, 400);
-        BarcodePanel bcpanel = new BarcodePanel();
-        add("Center", bcpanel);
+        setMinimumSize(new Dimension(600, 400));
+        Container contentPane = getContentPane();
 
-        try {
-            DefaultConfiguration cfg = new DefaultConfiguration("code128");
-            DefaultConfiguration child = new DefaultConfiguration("human-readable-font");
-            //child.setValue("OCR-B 10 Pitch BT");
-            child.setValue("Tahoma");
-            cfg.addChild(child);
+        JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        northPanel.add(new JLabel("Choose Barcode: "));
+        northPanel.add(getBarcodeNames());
+        northPanel.add(new JLabel("Message: "));
+        northPanel.add(getMessageField());
+        northPanel.add(new JLabel("Orientation: "));
+        northPanel.add(getOrientation());
+        contentPane.add(northPanel, BorderLayout.NORTH);
+        contentPane.add(getBarcodePanel(), BorderLayout.CENTER);
 
-            BarcodeGenerator gen
-                    = BarcodeUtil.getInstance().createBarcodeGenerator(cfg);
-
-            bcpanel.setBarcode(gen);
-            bcpanel.setMessage("Hello World!");
-        } catch (ConfigurationException e) {
-            LOGGER.log(Level.SEVERE, "Configuration Exception", e);
-        } catch (BarcodeException e) {
-            LOGGER.log(Level.SEVERE, "Barcode Exception", e);
-        }
+        getBarcodePanel().setBarcodeName((String) getBarcodeNames().getSelectedItem());
+        getBarcodePanel().setMessage(getMessageField().getText());
+        getBarcodePanel().setOrientation(getOrientation().getValue());
     }
 
     public void createAndShowGUI() {
         buildGUI();
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (getBarcodeNames().equals(e.getSource())) {
+            getBarcodePanel().setBarcodeName((String) getBarcodeNames().getSelectedItem());
+        }
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        if (getOrientation().equals(e.getSource())) {
+            getBarcodePanel().setOrientation(getOrientation().getValue());
+        }
     }
 }
